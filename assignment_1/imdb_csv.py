707f6ea1c6b57ec62f3b8498aee0964af1d8b447
Jagdeep Singh <jagdeep@webknot.in>
2024-03-15 16:02:36 +0530

    Add PDF generation for movies by director, language, and country

--- assignment_1/imdb_csv.py
+++ assignment_1/imdb_csv.py
@@ -25,7 +25,8 @@ with open('assignment_1/movies_initial.csv', 'r') as file:
         pdf.output("assignment_1/output_pdf/movies_by_director.pdf")
         return movies
     
-    # get_movies_by_director(rows, 'Nolan')
+
+    
 
     def get_movies_filtered_by_country(data, country):
         pdf = FPDF()
@@ -41,26 +42,42 @@ with open('assignment_1/movies_initial.csv', 'r') as file:
         pdf.output("assignment_1/output_pdf/movies_by_country.pdf")
         return movies
     
-    get_movies_filtered_by_country(rows, 'India')
+    
 
     def get_movies_by_language(data, language):
-        # language is 17 column
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font("Arial", size=12)
+        movies = []
+        pdf.cell(180, 10, txt=", ".join(header).encode('utf-8').decode('latin-1'), ln=True,border=True, align='C')
         movies = []
         for row in data:
-            if language in row[7]:
+            if language in row[6]:
                 movies.append(row)
-        print(movies)
+                pdf.cell(150, 10, txt=", ".join(row).encode('utf-8').decode('latin-1'), ln=True,)
+        pdf.output("assignment_1/output_pdf/movies_by_language.pdf")
         return movies
     
-    # get_movies_by_language(rows, 'Hindi')
+
 
     
     def get_movies_by_imdb_rating(data):
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font("Arial", size=12)
         movies = []
+        pdf.cell(180, 10, txt=", ".join(header).encode('utf-8').decode('latin-1'), ln=True,border=True, align='C')
         for row in data:
-            if row[11] != '' and float(row[4]) > 7 and row[5] != '' and int(row[12]) > 1000:
+            if row[4] != '' and float(row[4]) > 7 and row[5] != '' and int(row[5]) > 1000:
                 movies.append(row)
-        print(movies)
+                pdf.cell(150, 10, txt=", ".join(row).encode('utf-8').decode('latin-1'), ln=True,)
+        pdf.output("assignment_1/output_pdf/movies_by_imdb_rating.pdf")
         return movies
     
-    # get_movies_by_imdb_rating(rows)
+    get_movies_by_imdb_rating(rows)
+
+    get_movies_by_director(rows, 'Nolan')
+
+    get_movies_by_language(rows, 'Hindi')
+
+    get_movies_filtered_by_country(rows, 'India')
