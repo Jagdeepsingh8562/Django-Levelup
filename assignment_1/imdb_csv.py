74435ca2ac4a87fc369d6728de015ac8abb3edc4
Jagdeep Singh <jagdeep@webknot.in>
2024-03-15 16:06:35 +0530

    Add functions to filter movies by director, country, language, and IMDb rating

--- assignment_1/imdb_csv.py
+++ assignment_1/imdb_csv.py
@@ -1,17 +1,26 @@
 import csv
+import os
 from fpdf import FPDF
 
+# Create a directory to store the output pdf files
+if not os.path.exists('assignment_1/output_pdf'):
+    os.mkdir('assignment_1/output_pdf')
+
+# Read the csv file and store the data in a list
 with open('assignment_1/movies_initial.csv', 'r') as file:
     reader = csv.reader(file)
     header = next(reader)
+    # Selecting only the required columns
     header = [header[1],header[4],header[6],header[7],header[11],header[12],header[16],header[17]]
     
 
     rows = []
     for row in reader:
+         # Selecting only the required columns
         row = [row[1],row[4],row[6],row[7],row[11],row[12],row[16],row[17]]
         rows.append(row)
 
+    # Function to get movies by director
     def get_movies_by_director(data, director):
         pdf = FPDF()
         pdf.add_page()
@@ -27,7 +36,7 @@ with open('assignment_1/movies_initial.csv', 'r') as file:
     
 
     
-
+    # Function to get movies by country
     def get_movies_filtered_by_country(data, country):
         pdf = FPDF()
         pdf.add_page()
@@ -43,7 +52,7 @@ with open('assignment_1/movies_initial.csv', 'r') as file:
         return movies
     
     
-
+    # Function to get movies by language
     def get_movies_by_language(data, language):
         pdf = FPDF()
         pdf.add_page()
@@ -60,7 +69,7 @@ with open('assignment_1/movies_initial.csv', 'r') as file:
     
 
 
-    
+    # Function to get movies by imdb rating and number of votes
     def get_movies_by_imdb_rating(data):
         pdf = FPDF()
         pdf.add_page()
@@ -73,6 +82,8 @@ with open('assignment_1/movies_initial.csv', 'r') as file:
                 pdf.cell(150, 10, txt=", ".join(row).encode('utf-8').decode('latin-1'), ln=True,)
         pdf.output("assignment_1/output_pdf/movies_by_imdb_rating.pdf")
         return movies
+    
+
     
     get_movies_by_imdb_rating(rows)
 
